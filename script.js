d3f5630256e2a5e764503446ee4c3231b8335606
Martin Stone <martin-stone@users.noreply.github.com>
2023-05-25 08:38:42 +0100

    Note picker tweaks

--- script.js
+++ script.js
@@ -38,15 +38,15 @@
 
             function nextNote() {
                 const options = getOptions()
-                console.log(options);
 
                 if (source) {
                     source.stop()
                 }
 
-                const [noteNum, noteName, stringNum] = pickNextNote(options);
-                console.log(noteName);
-                document.getElementById("title").innerText = noteName;
+                const [noteNum, noteName, stringNums] = pickNextNote(options);
+                console.log(noteNum, noteName, stringNums);
+                document.getElementById("note").innerText = noteName;
+                document.getElementById("hint").innerText = options.stringHint ? `(string ${stringNums.join(", ")})` : "\u00A0";
                 source = playNote(noteNum, context);
 
                 timer = setTimeout(nextNote, options.periodSecs*1000);
@@ -67,27 +67,45 @@
     }
 
     function noteGen() {
-        const midiC0 = 12;
         const notes = "C2D4EF1G3A5B";
         const subscripts = "₀₁₂₃₄₅₆₇₈₉";
-        var inote = 0;
+        const stringNotes = [64, 59, 55, 50, 45, 40]; // from string 1 .. 6
+        const minNote = stringNotes[5];
+
+        var currentNote = minNote;
 
         function pickNextNote(options) {
-            // Don't repeat last note: inc by [1,12)
-            inote = (inote + randInt(1, 12)) % 12;
-            const ioctave = randInt(4, 7);
+            const noteRange = stringNotes[0] + options.maxFret - minNote;
+            // Don't repeat last note: inc by rand(1, note range-1) and wrap.
+            currentNote = (currentNote - minNote + randInt(1, noteRange)) % noteRange + minNote;
+            
+            return [
+                currentNote, 
+                nameFor(currentNote), 
+                stringsFor(currentNote, options)
+            ];
+        }
+
+        function nameFor(midiNote) {
+            const ioct = Math.floor(midiNote / 12) - 1;
+            const inote = midiNote % 12;
             var noteStr = notes.charAt(inote);
-            const sharpIndex = Number(notes.charAt(inote));
+            const sharpIndex = parseInt(notes.charAt(inote));
             if (!isNaN(sharpIndex)) {
+                // Tend towards whichever is the more common sharp or flat for a note:
                 const goSharp = randInt(1, 7) > sharpIndex;
                 // no need to wrap cos B/C have nothing between:
                 noteStr = notes.charAt(inote + (goSharp?-1:1)) + "♭♯".charAt(goSharp);
             }
-            return [
-                midiC0 + ioctave*12 + inote, 
-                noteStr + subscripts.charAt(ioctave), 
-                0
-                ];
+            return noteStr + subscripts.charAt(ioct);
+        }
+
+        function stringsFor(midiNote, options) {
+            console.log(options.maxFret);
+            return stringNotes
+                .map((n, i) => [n <= midiNote && midiNote <= n+options.maxFret, i+1])
+                .filter(pair => pair[0])
+                .map(pair => pair[1]);
         }
 
         return pickNextNote;
@@ -101,12 +119,9 @@
     function getOptions() {
         const inputs = [...document.querySelectorAll('table input')];
         const fromForm = Object.fromEntries(inputs.map(input => [
-            input.id, input.hasAttribute("checked") ? input.checked : input.value
+            input.id, input.hasAttribute("checked") ? input.checked : parseInt(input.value)
         ]));
-        const other = {
-            minNote: 40, // e2
-        };
-        return {...fromForm, ...other};
+        return fromForm;
     }
 
     function playNote(midiNote, context) {
@@ -131,21 +146,19 @@
 
     function createBuffer(context, midiNote) {
         const sr = context.sampleRate;
-        const bufferSamps = Math.round(sr * maxPeriodSecs);
-        const buffer = context.createBuffer(1, bufferSamps, sr);
-        writeNote(buffer.getChannelData(0), sr, midiNote);
+        const numBufferSamps = Math.round(sr * maxPeriodSecs);
+        const buffer = context.createBuffer(1, numBufferSamps, sr);
+        writeNote(buffer.getChannelData(0), numBufferSamps, sr, midiNote);
         return buffer;
     }
 
-    function writeNote(data, sr, midiNote) {
-        // Cosine blend from sharp 1 down to zero, based on clickToneFreq.
+    function writeNote(data, numSamps, sr, midiNote) {
         const toneFreq = midiNoteToFreq(midiNote);
         const periodSec = 1/toneFreq;
         const w = 2*Math.PI/periodSec;
-        const clickSamps = sr * maxPeriodSecs;
-        for (var i = 0; i < clickSamps; i++) {
+        for (var i = 0; i < numSamps; i++) {
             const tSec = i / sr;
-            const amplitude = 0.05/(tSec+0.05);
+            const amplitude = 0.5 * 0.05/(tSec+0.05);
             const a = 2*amplitude/Math.PI;
             const saw = a * Math.asin(Math.sin(w*tSec))
             data[i] = saw;
